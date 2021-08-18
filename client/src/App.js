ffdba9d87df02e5e983da234f26ce1c490ea9969
Gomathi-07 <gomathi.janakiram@banfico.com>
2021-08-18 22:48:39 +0530

    added route for home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
+import HomePage from './components/homePage/home'
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
+import Navbar from './components/navbar/navbar'
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">hi</header>
+      <header className="App-header">
+        <Navbar/>
+        <Router>
+          <Switch>
+            <Route exact path='/'>
+              <HomePage/>
+            </Route>
+          </Switch>
+        </Router>
+      </header>
     </div>
   );
 }
