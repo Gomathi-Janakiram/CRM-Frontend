d7d67897e4485070cdc2b87df61c805bc7252e15
Gomathi-07 <gomathi.janakiram@banfico.com>
2021-08-21 19:17:37 +0530

    added routes for add and view contact page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import HomePage from './components/homePage/home'
 import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
 import Navbar from './components/navbar/navbar'
+import AddContact from './components/addContact/contact'
+import Contacts from './components/contacts/contact'
 
 function App() {
   return (
@@ -11,6 +13,12 @@ function App() {
           <Switch>
             <Route exact path='/'>
               <HomePage/>
+            </Route>
+            <Route exact path='/addContact'>
+              <AddContact/>
+            </Route>
+            <Route exact path='/contacts'>
+              <Contacts/>
             </Route>
           </Switch>
         </Router>
