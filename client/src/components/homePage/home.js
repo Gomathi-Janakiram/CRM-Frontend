f215498e3b59b62a27af13759b2e98c0b48d0356
Gomathi-07 <gomathi.janakiram@banfico.com>
2021-08-21 19:16:13 +0530

    added home page

--- client/src/components/homePage/home.js
+++ client/src/components/homePage/home.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import './home.css'
+import PermContactCalendarSharpIcon from '@material-ui/icons/PermContactCalendarSharp';
+import { Typography,Button } from '@material-ui/core';
 
 const HomePage=()=>{
     return(
-        <h1>Hello</h1>
+        <div className='container'>
+            <PermContactCalendarSharpIcon className='contact-icon' color='primary'/>
+            <Typography color='primary' variant='h4'>Where Every Contact Counts!!!</Typography>
+            <section class='button-container'>
+                <Button href='/contacts' color='primary' variant='contained'>VIEW</Button>
+                <Button href='/addContact' color='primary' variant='contained'>CREATE</Button>
+            </section>
+        </div>
     )
 }
 
