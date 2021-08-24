b603127ce5ba6564dc0dffa7ff991c0d48580422
Gomathi-07 <gomathi.janakiram@banfico.com>
2021-08-24 20:10:20 +0530

    implemented create contact page

--- client/src/components/addContact/contact.js
+++ client/src/components/addContact/contact.js
@@ -1,10 +1,55 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import {TextField,Typography,Paper,Button} from '@material-ui/core'
+import './contact.css'
 
 
 const AddContact=()=>{
+
+    const [data,setData]=useState({
+        firstName:"",
+        lastName:"",
+        email:"",
+        company:"",
+        Phone:""
+    })
+
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+        console.log(data)
+        postData(data)
+    }
+
+    const postData=(data)=>{
+        fetch("http://localhost:4000/contact",{
+            method:"post",
+            headers:{
+                "content-type":"application/json"
+            },
+            body:JSON.stringify(data)
+        })
+        .then(result=>{
+            return result.json()
+        })
+        .then((res)=>{
+            console.log(res)
+        })
+    }
+
     return(
-        <h1>Add you contact herer!!!</h1>
+        <div className='container'>
+            <Paper>
+                <Typography variant='h4' color='primary' className='heading'>Create New Contact</Typography>
+                <form className='form-wrapper' autoComplete='off' onSubmit={handleSubmit}>
+                    <TextField label='First Name' variant='outlined' className='fields' onChange={(e)=>setData({...data,firstName:e.target.value})}/>
+                    <TextField label='Last name' variant='outlined' className='fields' onChange={(e)=>setData({...data,lastName:e.target.value})}/>
+                    <TextField label='Email Id' variant='outlined' className='fields' onChange={(e)=>setData({...data,email:e.target.value})}/>
+                    <TextField label='Company' variant='outlined' className='fields' onChange={(e)=>setData({...data,company:e.target.value})}/>
+                    <TextField label='Contact Number' variant='outlined' className='fields' onChange={(e)=>setData({...data,Phone:e.target.value})}/>
+                    <button type='submit' className='create-btn'>Create</button>
+                </form>
+            </Paper>
+        </div>
+       
     )
 }
 
