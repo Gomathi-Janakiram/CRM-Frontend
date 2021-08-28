0b4897804d46a837abf689dc3be83263b71cc9a3
Gomathi-07 <gomathi.janakiram@banfico.com>
2021-08-29 00:26:17 +0530

    state management of input fields

--- client/src/components/LoginPage/login.js
+++ client/src/components/LoginPage/login.js
@@ -4,18 +4,25 @@ import {Paper,Typography,Button,TextField} from '@material-ui/core'
 import './login.css'
 
 const LoginPage=()=>{
+    const [email,setEmail]=useState('')
+    const [password,setPassword]=useState('')
+    const [errorMessage,setErrorMessage]=useState('')
+
+    const history=useHistory()
+
     return(
         <div className='container'>
             <Paper>
             <Typography variant='h2' color='primary' className='heading'>Log In</Typography>
+            {errorMessage && <div>{errorMessage}</div>}
             <form className='form-wrapper'>
-                <TextField className='fields' label='Email' variant='outlined' required='true'/>
-                <TextField className='fields' label='Password' variant='outlined' required='true'/>
+                <TextField className='fields' label='Email' variant='outlined' required='true' onChange={e=>setEmail(e.target.value)}/>
+                <TextField className='fields' label='Password' variant='outlined' required='true' onChange={e=>setPassword(e.target.value)}/>
                 <div className='button-wrapper'>
-                    <Button variant='contained' color='primary'>Log In</Button>
-                    <Button>Forgot Password?</Button>
+                    <Button variant='contained' color='primary' disabled={!email| !password}>Log In</Button>
+                    <Button onClick={()=>history.push('/forgot-password')}>Forgot Password?</Button>
                 </div>
-                <p className='signup-btn'>Don't have an account? <Button>Sign Up</Button></p>      
+                <p className='signup-btn'>Don't have an account? <Button onClick={()=>history.push('/signup')}>Sign Up</Button></p>      
             </form>
         </Paper>
         </div>
